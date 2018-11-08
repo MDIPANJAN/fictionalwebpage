90475b8556a03314e3ea9b5b80b253b43a70b23a
mdipanjan <mdipanjan33@gmail.com>
2018-11-07 17:53:03 -1200

    First commit

---
+++ dist/app.js
@@ -0,0 +1,24 @@
+// function hamburger(){
+//     let x = document.getElementById('myButton');
+//     if(x.className === 'topnav'){
+//         x.className += ' responsive';
+//     }else{
+//         x.className = 'topnav'
+//     }
+// }
+
+
+
+function hamburger(){
+    let x = document.getElementById("myButton");
+    if(x.className === "topnav"){
+        x.className += " responsive";
+    }
+    else{
+        x.className = "topnav";
+        
+    }
+
+}
+
+
